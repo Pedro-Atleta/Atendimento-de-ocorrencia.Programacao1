1b940cc661f7d01fba5b8fb49685e1dbf44486e2
aluno <aluno@lab01-14>
2026-05-05 14:03:08 -0300

    commit1

---
+++ ArendimentoDeOcorrencia.Programacao1/src/main/java/Bombeiro.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author aluno
+ */
+public class Bombeiro {
+ private int IdBombeiro;
+    private String Nome;
+    private String NomeDeGuerra;
+    private int Matricula;
+    private int Contato;
+
+    public int getIdBombeiro() {
+        return IdBombeiro;
+    }
+
+    public void setIdBombeiro(int IdBombeiro) {
+        this.IdBombeiro = IdBombeiro;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getNomeDeGuerra() {
+        return NomeDeGuerra;
+    }
+
+    public void setNomeDeGuerra(String NomeDeGuerra) {
+        this.NomeDeGuerra = NomeDeGuerra;
+    }
+
+    public int getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(int Matricula) {
+        this.Matricula = Matricula;
+    }
+
+    public int getContato() {
+        return Contato;
+    }
+
+    public void setContato(int Contato) {
+        this.Contato = Contato;
+    }
+}
